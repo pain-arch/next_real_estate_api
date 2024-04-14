25201372f49d47d75217e0bbf1202bd7b5d39fff
pain-arch <himulol123@gmail.com>
2024-04-14 16:09:18 +0600

    Add authentication controller functions in auth.controller.js

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+    //db logic here
+}
+
+export const login = (req, res) => {
+    //db logic here
+}
+
+export const logout = (req, res) => {
+    //db logic here
+}
