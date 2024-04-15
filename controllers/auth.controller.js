577d314d8ba40eef68e370731facaea80462bcc3
pain-arch <himulol123@gmail.com>
2024-04-15 15:04:02 +0600

    try catch error functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,20 +4,26 @@ import prisma from '../lib/prisma.js';
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
 
-    //Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
+    try {
+        //Hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
 
-    //Save user to database
-    const newUser = await prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-        },
-    });
+        //Save user to database
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
 
-    console.log(newUser);
+        console.log(newUser);
+        res.status(201).json({ message: "User created successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to create user" });
+    }    
 };
 
 export const login = (req, res) => {
