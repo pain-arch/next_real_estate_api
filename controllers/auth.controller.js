9f0365162046d93fa79e0afa9035accf63e16ca0
pain-arch <himulol123@gmail.com>
2024-04-15 14:40:29 +0600

    Add user registration functionality and add prisma/client pakage

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
@@ -6,6 +7,17 @@ export const register = async (req, res) => {
     //Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
+
+    //Save user to database
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    console.log(newUser);
 };
 
 export const login = (req, res) => {
