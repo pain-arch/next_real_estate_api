cdbe46661a71f3222392313a6df6b0698072d32a
pain-arch <himulol123@gmail.com>
2024-04-26 18:05:32 +0600

    Refactor authentication routes to use POST method in auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,8 +3,8 @@ import { login, logout, register } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get('/register', register);
-router.get('/login', login);
-router.get('/logout', logout);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
